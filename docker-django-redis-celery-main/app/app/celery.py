89280b3108dd9f0156478af21fe10ab0e2e5af7b
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-18 04:49:34 +0200

    Update celery.py

--- docker-django-redis-celery-main/app/app/celery.py
+++ docker-django-redis-celery-main/app/app/celery.py
@@ -13,7 +13,9 @@ app.autodiscover_tasks()
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
+    
     # run every hour (60 minutes) to fetch prices from alpha vantage api
+    
     sender.add_periodic_task(
         crontab(minute='*/60'),
         test.s(),
