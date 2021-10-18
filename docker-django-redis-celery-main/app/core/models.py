93b015e9706112087f55e506d975440244eb40df
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-18 04:49:08 +0200

    Update models.py

--- docker-django-redis-celery-main/app/core/models.py
+++ docker-django-redis-celery-main/app/core/models.py
@@ -5,3 +5,4 @@ class ExchangeRate(models.Model):
     from_currency_name = models.CharField(blank=True, max_length=255)
     to_currency_name = models.CharField(blank=True, max_length=255)
     exchange_rate = models.DecimalField(max_digits=8)
+
